5ff8525151df1aa033acfc134eed5fc8794e5543
Frantisek Stefl <st61461@upce.cz>
2019-09-30 21:40:48 +0200

    validate vehicle identifier

--- src/main/java/cz/upce/fei/inptp/zz/entity/Vehicle.java
+++ src/main/java/cz/upce/fei/inptp/zz/entity/Vehicle.java
@@ -10,20 +10,27 @@ package cz.upce.fei.inptp.zz.entity;
  * @author Roman
  */
 public class Vehicle {
-    private String identifier; // id should not be null
+    private String identifier;
     private int capacity; // capacity should be nonzero and positive number
     private VehicleType type;
     // TODO:initial position?
 
     public Vehicle(String identifier, int capacity, VehicleType type) {
-        this.identifier = identifier;
+        setIdentifier(identifier);
         this.capacity = capacity;
         this.type = type;
     }
-
-    
+  
     public String getIdentifier() {
         return identifier;
+    }
+
+    private void setIdentifier(String identifier) {
+        if ((identifier == null) || (identifier.isEmpty())) {
+            throw new NullPointerException("id should not be null!");
+        }
+
+        this.identifier = identifier;
     }
 
     public int getCapacity() {
