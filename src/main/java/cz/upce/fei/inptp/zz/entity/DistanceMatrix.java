f5ad39c34db009e17dca9fe9efbbc1698c6017f5
Michal Struna <michal.l.struna@gmail.com>
2019-10-03 14:00:53 +0200

    Refactor DistanceMatrix.

--- src/main/java/cz/upce/fei/inptp/zz/entity/DistanceMatrix.java
+++ src/main/java/cz/upce/fei/inptp/zz/entity/DistanceMatrix.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package cz.upce.fei.inptp.zz.entity;
 
 /**
@@ -20,25 +15,20 @@ public class DistanceMatrix {
     }
     
     public double get(String from, String to) {
-        int f, t;
-
-        f = getLocationIndex(from);
-        t = getLocationIndex(to);
+        int fromIndex = getLocationIndex(from);
+        int toIndex = getLocationIndex(to);
         
-        return distances[f][t];
+        return distances[fromIndex][toIndex];
     }
 
-    private int getLocationIndex(String value){
+    private int getLocationIndex(String value) {
         for (int i = 0; i < locations.length; i++) {
-            String location = locations[i];
-
-            if (location.equals(value)) {
+            if (locations[i].equals(value)) {
                 return i;
             }
         }
+
         return -1;
     }
-    
-    
     
 }
