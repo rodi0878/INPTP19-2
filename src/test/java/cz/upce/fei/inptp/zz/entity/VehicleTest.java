99aa3a89710be39858b259fb5d6b3a33116faa85
Tomáš Křičenský <st52530@student.upce.cz>
2019-10-06 22:29:19 +0200

    Refactor and test vehicle

---
+++ src/test/java/cz/upce/fei/inptp/zz/entity/VehicleTest.java
@@ -0,0 +1,40 @@
+package cz.upce.fei.inptp.zz.entity;
+
+import org.junit.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ *
+ * @author st52530
+ */
+public class VehicleTest {
+
+    @Test(expected = NullPointerException.class)
+    public void testInvalidIdentifier() {
+        Vehicle vehicle = new Vehicle(null, 5, VehicleType.Small);
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void testNegativeCapacity() {
+        Vehicle vehicle = new Vehicle("id", -5, VehicleType.Small);
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void testZeroCapacity() {
+        Vehicle vehicle = new Vehicle("id", 0, VehicleType.Large);
+    }
+
+    @Test(expected = NullPointerException.class)
+    public void testInvalidType() {
+        Vehicle vehicle = new Vehicle("id", 5, null);
+    }
+
+    @Test
+    public void testVehicleCreation() {
+        Vehicle vehicle = new Vehicle("id", 5, VehicleType.Medium);
+        assertEquals("id", vehicle.getIdentifier());
+        assertEquals(5, vehicle.getCapacity());
+        assertEquals(VehicleType.Medium, vehicle.getType());
+    }
+    
+}
