c619999a11a96ec1bfd8a62122bbf7481ca91c6a
Dominik Janák <mail@dominikjanak.cz>
2019-10-07 18:56:12 +0200

    Remove Order comments

--- src/main/java/cz/upce/fei/inptp/zz/entity/Order.java
+++ src/main/java/cz/upce/fei/inptp/zz/entity/Order.java
@@ -1,14 +1,5 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package cz.upce.fei.inptp.zz.entity;
 
-/**
- *
- * @author Roman
- */
 public class Order {
     private String from;
     private String to;
