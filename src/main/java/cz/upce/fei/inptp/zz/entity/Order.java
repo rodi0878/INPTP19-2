cb32e410a89534218c5f0e6b89a1068854b28f87
Dominik Janák <mail@dominikjanak.cz>
2019-10-07 18:52:48 +0200

    Order constructor added

--- src/main/java/cz/upce/fei/inptp/zz/entity/Order.java
+++ src/main/java/cz/upce/fei/inptp/zz/entity/Order.java
@@ -14,5 +14,14 @@ public class Order {
     private String to;
     private int capacity; // see vehicle capacity
     //TODO: add ability to custom filter allowed vehicles - by type, by specific number
+
+    public Order(String from, String to, int capacity) {
+        this.from = from;
+        this.to = to;
+        if (capacity <= 0) {
+            throw new IllegalArgumentException("Capacity should be non-zero and positive number. Received " + capacity);
+        }
+        this.capacity = capacity;
+    }
             
 }
