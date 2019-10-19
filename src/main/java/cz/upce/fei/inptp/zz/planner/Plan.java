21b897c61a53e671a892a960999c7c904e5816fe
Roman Diviš <divisak@gmail.com>
2019-10-19 20:27:42 +0200

    Plan updates and first version of planitem price calculator

--- src/main/java/cz/upce/fei/inptp/zz/planner/Plan.java
+++ src/main/java/cz/upce/fei/inptp/zz/planner/Plan.java
@@ -1,8 +1,29 @@
 package cz.upce.fei.inptp.zz.planner;
 
+import cz.upce.fei.inptp.zz.entity.Driver;
+import cz.upce.fei.inptp.zz.entity.Vehicle;
 import java.util.ArrayList;
+import java.util.List;
 
 public class Plan {
     
-    private ArrayList<PlanItem> plan;
+    private List<PlanItem> plan;
+
+    public Plan() {
+        plan = new ArrayList<>();
+    }
+
+    public List<PlanItem> getPlan() {
+        return plan;
+    }
+    
+    public PlanItem addNewVehiclePlan(Vehicle vehicle, Driver driver) {
+        PlanItem newplan = new PlanItem(vehicle, driver);
+        
+        plan.add(newplan);
+        // TODO: check duplicity of vehicle/driver in plan
+        
+        return newplan;
+    }
+    
 }
