21c03bec6459bcd252fc214a13d33662c867762d
st52523 <st52523@student.upce.cz>
2019-10-21 10:24:36 +0200

    Update DistanceMatrixLoader.java
    Import knihoven

--- src/main/java/cz/upce/fei/inptp/zz/entity/DistanceMatrixLoader.java
+++ src/main/java/cz/upce/fei/inptp/zz/entity/DistanceMatrixLoader.java
@@ -5,6 +5,8 @@ import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.Arrays;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 public class DistanceMatrixLoader {
 
